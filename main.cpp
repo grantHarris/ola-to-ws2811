88aaeacfc69e4958bc4ff4b2e65bfe7c52050a66
Grant Harris <grant@grantharris.ca>
2017-10-04 17:04:20 +0000

    Library update

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@
 #include "lib/pwm.h"
 #include "lib/version.h"
 #include "lib/ws2811.h"
-//#include "lib/yaml.h"
+#include "yaml-cpp/yaml.h"
 
 #define TARGET_FREQ             WS2811_TARGET_FREQ
-#define GPIO_PIN                21
 #define DMA                     5
 #define STRIP_TYPE            	WS2811_STRIP_GRB
 
 ws2811_t output;
+YAML::Node config;
 
 void RegisterComplete(const ola::client::Result& result) {
 	if (!result.Success()) {
@@ -25,51 +25,78 @@ void RegisterComplete(const ola::client::Result& result) {
 }
 
 void Receive(const ola::client::DMXMetadata &metadata, const ola::DmxBuffer &data) {
+
 	ws2811_return_t ret;
-	
-	for(std::size_t i = 0; i < data.Size(); i++){
-		output.channel[0].leds[i] = (uint32_t) data.Get(i * 3) << 16 |
-		(uint32_t) data.Get(i * 3 + 1) << 8 |
-		(uint32_t) data.Get(i * 3 + 2);
+	int strip_channel, start_address;
+
+	for(YAML::const_iterator it=config["mapping"][metadata.universe].begin(); it!=config["mapping"][metadata.universe].end(); ++it) {
+		std::cout << "hey there!" << it->second <<std::endl;
+
 	}
 
-	if ((ret = ws2811_render(&output)) != WS2811_SUCCESS){
-		std::cout << "ws2811_render failed:" << ws2811_get_return_t_str(ret);
-	}
+	// for(YAML::const_iterator it = config["mapping"][metadata.universe].begin(); it != config["mapping"][metadata.universe].end(); ++it) {
+	// 	int strip_channel = it->second["output"]["strip_channel"].as<int>();
+	// 	std::cout<<strip_channel;
+	// 	// int start_address = std::max(0, std::min(it->second["output"]["start_address"].as<int>() - 1, 511));
+	// 	// int total_rgb_channels = std::max(0, std::min(it->second["output"]["total_rgb_channels"].as<int>() - 1, 511));
+
+	// 	// int end_address = std::max(0, std::min(total_rgb_channels - start_address, 511));
+
+	// 	// for(std::size_t i = start_address; i < end_address; i++){
+	// 	// 	output.channel[strip_channel].leds[i] = (uint32_t) data.Get(i * 3) << 16 |
+	// 	// 	(uint32_t) data.Get(i * 3 + 1) << 8 |
+	// 	// 	(uint32_t) data.Get(i * 3 + 2);
+	// 	// }
+	// }
+
+	// if ((ret = ws2811_render(&output)) != WS2811_SUCCESS){
+	// 	std::cout << "ws2811_render failed:" << ws2811_get_return_t_str(ret);
+	// }
 }
 
 int main() {
-//	YAML::Node config = YAML::LoadFile("config.yaml");
-	
+	config = YAML::LoadFile("config.yaml");
 	ws2811_return_t ret;
 	
 	output.freq = TARGET_FREQ;
 	output.dmanum = DMA;
-	output.channel[0].gpionum = GPIO_PIN;
-	output.channel[0].count = 670;
-	output.channel[0].invert = 0;
-	output.channel[0].brightness = 255;
-	output.channel[0].strip_type = STRIP_TYPE;
 
-	if ((ret = ws2811_init(&output)) != WS2811_SUCCESS)
-		std::cout << "ws2811_init failed:" << ws2811_get_return_t_str(ret);
-		exit(ret);
+	for(YAML::const_iterator it=config["strip_channel"].begin(); it!=config["strip_channel"].end(); ++it) {
+  		int ch = it->first.as<int>();
+
+  		int gpio_pin = it->second["gpionum"].as<int>();
+  		int count = it->second["count"].as<int>();
+  		int invert = it->second["invert"].as<int>();
+  		int brightness = it->second["brightness"].as<int>();
+
+  		output.channel[ch].gpionum = gpio_pin;
+		output.channel[ch].count = count;
+		output.channel[ch].invert = invert;
+		output.channel[ch].brightness = brightness;
+		output.channel[ch].strip_type = STRIP_TYPE;
+	}
+
 	
+	if ((ret = ws2811_init(&output)) != WS2811_SUCCESS){
+		std::cout << "ws2811_init failed:" << ws2811_get_return_t_str(ret);
+		exit(1);
+	}
 
 	ola::InitLogging(ola::OLA_LOG_INFO, ola::OLA_LOG_STDERR);
 	ola::client::OlaClientWrapper wrapper;
-
-	if (!wrapper.Setup())
+	
+	if (!wrapper.Setup()){
+		std::cout << "OLA client wrapper failed.";
 		exit(1);
+	}
 
 	ola::client::OlaClient *client = wrapper.GetClient();
-
 	client->SetDMXCallback(ola::NewCallback(&Receive));
 
-//	for(std::size_t i = 0; i< config["mapping"].size(); i++){
-//		client->RegisterUniverse(config["mapping"][i]["universe"].as<int>(), ola::client::REGISTER, ola::NewSingleCallback(&RegisterComplete));
-//	}
-	
-	wrapper.GetSelectServer()->Run();
-	                              
+	for(YAML::const_iterator it=config["mapping"].begin(); it!=config["mapping"].end(); ++it) {
+  		int universe = it->first.as<int>();
+  		client->RegisterUniverse(universe, ola::client::REGISTER, ola::NewSingleCallback(&RegisterComplete));
+	}
+
+	wrapper.GetSelectServer()->Run();                            
 }
